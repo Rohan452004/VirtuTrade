2ab7ea5a8f101b80041915f7f7494f14a24fa7b9
Rohan Agrawal <104892210+Rohan452004@users.noreply.github.com>
2025-03-12 19:30:28 +0530

    File Structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+// import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className='text-2xl font-bold text-center'>Hello VirtuTrade</p>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
